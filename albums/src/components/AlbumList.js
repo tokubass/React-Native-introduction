2a996face4111811768fd098fb7e6e31512c0ef1
tokubass <tomi21110@gmail.com>
2017-06-10 16:08:38 +0900

    cardsection #46

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { View,Text  } from 'react-native';
+import { View  } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+    
     componentWillMount(){
         //console.log('componentWillMount in AlbumList');
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums : response.data }));
+    }
+
+    renderAlbums(){
+        // AlbumDetailの引数propsにAlbumのstateを渡す album={album}
+        return this.state.albums.map(album =>
+           <AlbumDetail key={album.title} album={album} />
+        );
     }
     
     render () {
         return (
             <View>
-                <Text>album list!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     };
