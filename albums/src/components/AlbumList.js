4e56e03d0dbd058c8a61d22bd379ed4fbccff1c9
tokubass <tomi21110@gmail.com>
2017-06-10 14:42:04 +0900

    update

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View,Text  } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    componentWillMount(){
+        //console.log('componentWillMount in AlbumList');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response));
+    }
+    
+    render () {
+        return (
+            <View>
+                <Text>album list!!</Text>
+            </View>
+        );
+    };
+};
+
+export default AlbumList;
